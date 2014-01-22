d23a3b770283c80ff35d5f74d0a8f9a2daa48896
Stephanie Sharp <stephanie@ruadvertising.com.au>
2014-01-22 15:40:31 +1100

    Override +JSONKeyPathsByPropertyKey in WXDailyForecast

--- SimpleWeather/WXDailyForecast.m
+++ SimpleWeather/WXDailyForecast.m
@@ -10,4 +10,17 @@
 
 @implementation WXDailyForecast
 
++ (NSDictionary *)JSONKeyPathsByPropertyKey
+{
+    // Get WXCondition‘s map and create a mutable copy of it
+    NSMutableDictionary *paths = [[super JSONKeyPathsByPropertyKey] mutableCopy];
+
+    // Change the max and min key maps to what you’ll need for the daily forecast
+    paths[@"tempHigh"] = @"temp.max";
+    paths[@"tempLow"] = @"temp.min";
+
+    // Return the new mapping
+    return paths;
+}
+
 @end
